3e577dc52e2a2637fce51382b55f1cfed17f262d
tejas-contentstack <132910969+tejas-contentstack@users.noreply.github.com>
2024-08-07 21:52:23 +0530

    Remove functions

--- functions/hello-world.js
+++
@@ -1,6 +0,0 @@
-// functions/hello.js
-
-export default function handler(request, response) {
-    console.log('hey there');
-    return response.status(200).json({message: "Hello world from cloud function!"});
-  }
